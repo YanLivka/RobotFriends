138c6ab03e8cb29cd69bda1700113557d355c94f
Yan Livenu <yan@wildsparq.com>
2021-11-13 17:11:58 -0600

    Updates

---
+++ precache-manifest.4de99f357339b755e2d32f1ab1f88f12.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e8e0c67d32b5b3218f52496d1ab171",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "65fd17fa04a298162015",
+    "url": "/robotfriends/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "19c9abd53c345e5bfa7c",
+    "url": "/robotfriends/static/css/main.da34b415.chunk.css"
+  },
+  {
+    "revision": "65fd17fa04a298162015",
+    "url": "/robotfriends/static/js/2.198822f0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/robotfriends/static/js/2.198822f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1defe4f2415ca039d1b",
+    "url": "/robotfriends/static/js/3.9d47cd19.chunk.js"
+  },
+  {
+    "revision": "19c9abd53c345e5bfa7c",
+    "url": "/robotfriends/static/js/main.42599bb7.chunk.js"
+  },
+  {
+    "revision": "5b5155b487d1c4bd3e73",
+    "url": "/robotfriends/static/js/runtime-main.db2eea9e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
